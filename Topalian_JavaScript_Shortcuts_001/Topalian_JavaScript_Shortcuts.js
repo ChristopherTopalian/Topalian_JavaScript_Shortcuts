7656a6568657312ee33eacc3dd85b5146d218af0
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2023-12-13 21:35:13 -0500

    Add files via upload

---
+++ Topalian_JavaScript_Shortcuts_001/Topalian_JavaScript_Shortcuts.js
@@ -0,0 +1,25 @@
+// Dedicated to God the Father
+
+// All Rights Reserved Christopher Topalian Copyright 2000-2023
+
+// https://github.com/ChristopherTopalian
+
+// https://github.com/ChristopherAndrewTopalian
+
+// Topalian_JavaScript_Shortcuts.js
+
+// Version 001
+
+function ge(whichElement)
+{
+    let result = document.getElementById(whichElement);
+
+    return result;
+}
+
+function ce(whichType)
+{
+    let result = document.createElement(whichType);
+
+    return result;
+}
